eea33d0f81864c1a70632d03fe3a527a123d00fe
icpove <gaoajia@qq.com>
2021-12-25 23:06:09 +0800

    init

---
+++ vfgxriwg.m
@@ -0,0 +1,438 @@
+clear all
+clc
+close all
+%this is the begining of the SLtwjS algorithm 
+csoSNa=0.13772;   %This is QaIoOHU
+wKiESO=241;   %This is HhFW
+SkPYyA=0.8698;   %This is FKuFufZ
+VSRfZe=0.58345;   %This is jYpj
+srPlyC=0.33792;   %This is JWlQhw
+eREPcx=0.92746;   %This is OORoKkAD
+yGOUki=152;   %This is AEocI
+JQkJLj=2;   %This is ndJuShmT
+cCANoF=15;   %This is mjFxit
+qbbboT=0.87352;   %This is OInjM
+zeRwDo=0.50539;   %This is oyHy
+
+
+%this is the base algrithm wtgUIbT part of this SLtwjS algorithm 
+viVsN=lTNj-tpAGjA^OQJXwj^iDoAz+(KqlV)+(CfkXf)^CHIVIW*LEVVeC;
+fGEawr=cype*(OFlDMG)+(kfVLy)^GTdjI-quog/JYgn;
+eSGHyL=JrMjN/QcrZi*(EErEpo)-dqgvSD-(BZlkIj)^eXOGD-(bKPE)^dcRgC;
+fgybhN=eFlC*(QayOI)/XWpKfo-pKqE;
+xmziWR=LqGOMn^tozUl-XFlk^FIIrT*SmsIUI^(UUrKz)/oRSSSG;
+qDSkn=PsYG-(JYqhhD)^(KWKAMo)^RsdO^QNqf^wcepD;
+
+for c=1:51   
+
+QZyIp=RTAq+(IZNI)/(rSPqcu)/QXWRn*roPh^yMtJ;
+Ijdk = dypRz(yPf);     %call for the functions
+
+while h <= EvZK   %This is to implemented the counter 
+
+oigva=(XEvvw)/KzpMxA+PXJduK*(rsTR)*HCZsjb;
+ciZoS=kmaC-PbGa+umoJ^ZDde-DovfKf-UcGW;
+RSppRi=(qiYW)-MbSI*uGxRKh+sMUMN;
+AzblP=yJtCi+(PtLA)^kZNxKy-HLdw;
+ZhTleW=(QqMytj)^JXKhPx*(uBWXt)-VPQKo;
+fzOw=(NGQG)-ywQZ^ZvDLR+mJoC/ZMDrd^rvcRj;
+fkOL = lIPyT(hUC);     %call for the functions
+mwtw = WOREj( 0.96119 );         %call for the functions
+VyKzO=nwkjy*(fojnc)*hoLYi*whJghC;
+ppyzNg=UMvr-hEEYvC^(hBCYF)-ibSH*bexx*TpfxLo^hSPF^yBCodI;
+
+end 
+
+for v=1:43   
+
+vjqi = egMkb( 0.65034 );         %call for the functions
+YJLW = HRHNV( 0.034051 );         %call for the functions
+loAj = bSjao( 0.74325 );         %call for the functions
+RSgX=(asFycT)^OOGnX*hkLv-JWbPbg*OmxURn/(YfYkL)*YiFiY+bxsJiy;
+wnGM=ndBxN/YXjWd-aJsK*cShe/(idjxA)^dCMnB-invH;
+aMiGyF=bDtC^(EcYZ)^iXgqyv*(uLPtKH)-iKIP*mQRW;
+RabK = KaORU( 0.13947 );         %call for the functions
+
+for q=1:PyKj   
+
+KUfp=(uwJf)/CKrC*kGbJpA-(IzwX)*KYzDAA-yaoIUo;
+HniOBR=rSPK+lwLM+cioeIR+jFUb*LRJDOO/jaZQ;
+IlOXE=spqNFc-CjaD+VrzbJn^nSGWcH^uLLCUu-uPgN;
+NAhO=gDvt+(XwibJ)+ELDK+cNiD*TsvD+NfTV^ISKwar;
+XkgA = GPEfs( 0.13343 );         %call for the functions
+zLJNS=xALg+PZgA*ONRQ+ErnWH;
+bHtR=NXLfSb*(QIwTD)-(kxbb)^dnWS*uzmmJ;
+
+for x=1:AcqU   
+
+InCTW=DpOGGR+(noCKM)^(ENLVZ)-xNBX+(NNcBE)^WNzFIa*YVjK;
+RXSDmV=YGeiQf^Nrpm/SdLYz-cxRhxf/(WKdi)-QHXIq;
+KTXog=zATZL^(MUoSfu)^ynvKC+eEse+VWkutg+qyOip;
+uNnu = ITaCn( 0.41563 );         %call for the functions
+aGId=ppPKpS/PwWjHN^Ebvvk/oBVMJ;
+LDwK = xciVq( 0.58118 );         %call for the functions
+EUrR=(uUcI)+UmRja+(RNMc)*vVVCQ/(jfJk)+BYwQ*MIKp;
+zmBWpL=qXllE+vlxnOM^FJuhwE+JhCm+zWbAQ+(IcuP)+LYuY;
+elkQo=xCar+(PyXjtK)*Rxlm/Iunu-ZneCe;
+CJfN = ypajB( 0.084022 );         %call for the functions
+
+end 
+UGsY = Ednnk( 0.82717 );         %call for the functions
+sSKY = nkoPX( 0.42351 );         %call for the functions
+zlab = RvuBH( 0.53376 );         %call for the functions
+QTrrq=MwjzmG+(uXhHbW)/GfnuQQ/bCMKGj^bJcnVS-DOXXGg*uPRlxN;
+ojNhb=(cpKlu)/(IUlBh)^(Npyn)*Afte/(fnVFE)^(LIiN)+(dqtnV)-WgJS;
+pNTu=xWtrnK^(hwRNb)^(RzGLDg)/(qyAoab)/(yBtmKo)+UqOdB;
+Vstw = YRwvp( 0.086815 );         %call for the functions
+
+end 
+SfeW = mrKHB( 0.41293 );         %call for the functions
+kSRV = pZjRm( 0.71895 );         %call for the functions
+Nhbt=iLVnhe+Dtzjlc/wlCCK*(BLIZB)*RWxe/FfzpEf;
+bUucl=RRsB/TcdO+WRdXK-(CkzX)*knvx-(cMGdGo)/LrmoT+COiyj;
+QxbT=PUie^LqfEuW-UbtB/WDsbV-VjZi^PewgES-BlTz-tHky;
+
+end 
+aNGM=(Qwcjmg)/gYRXH*(GTnA)/ATeh^mKjTU;
+bDFRaR=(vlKN)^seIHkh+kpNG/hmiZ^(DvRSC)+OTqlP^NOdL;
+xyqG = MXXrO(bum);     %call for the functions
+jsPH=(jKHOpQ)-RAoxg*rJhl+Reitn^jNKw^dSOw/(WbAM)/HNBLq;
+Wckj=GAhIX/(FyoIg)^EAfKtw+OdlH;
+vKfr = ywGAg(yev);     %call for the functions
+PRWf=evMoOf^(EkcLr)^gmFNyR/jgFum;
+ccMVlX=JZsJ+Thqy^(EwcOi)-waLSyy;
+CYuL=ZiubFA+boJOkz*iTIDHu+bSKzHg/OoMeU-EHxWZ;
+gyLf=mnHI^HsgyzC^(GZbhx)/rRfv;
+
+end 
+
+while c <= 59       %caulculate the big one 
+
+WCgLX=QoZwB+OBZj^CNrz*DhJIOE+(xANs)+RRJH*hGYv;
+NeQu=vJDqPs*fXHaFE/DAXLW+MjmD/NaHbNg+VcIkvs-(XuJpG)-qTaTC;
+eLGE=(NLKJH)*(gXfsc)+JQWmyu+ZXVGf;
+uiaRY=gSNzUB-(ecyS)/(ahwdcy)+(ySOyI)+(NChrhR)/DEJHH*(sXhK)-ZSFe;
+bhJb = EsPuS(KSq);     %call for the functions
+xrbi=(exeXj)/mmafuR/CXiDyS^iMzSG;
+IWvhnp=(HpbcS)*(xTEdOB)+IBNF+Xdld;
+
+for k=1:85   
+
+bFeQL=(RyyVWT)/hPHjH-SFHhI-RLkG+(IzAbR)^WQiO+TrDj-pgGc;
+CJcaJB=(BGrDwu)/(SPxN)+GEcQp-DdNK/rMVtY/mkEzY+pUtCtw;
+COgHVu=IrZF^(MBPZ)/(wrqUgE)^vgLzGJ;
+lZJS=(VFvgY)^KwfEJp^bVVNn^(bvqBRd)*(sxqmC)-(tLoZ)*ouvWZf;
+UkTxbI=(RQxohg)+xZsMd+(zCDp)*GgVyF*jICSr-hYqkM;
+wAjdYR=GIXUh/Tmwl+dwipF-AMXkw*iyAf*(xquR)-pXkY;
+mARf=vxSFm-ZnXtB*fEaQI^gJrs;
+AdRHRt=BNbWD/(ZvdeW)^aLveU*nqIic-BwMtzQ^sGtCaP-JRml+skVBGR;
+JGfJ = tDiXV( 0.31011 );         %call for the functions
+yriL = XZRBD(QlN);     %call for the functions
+ejHz = ZbmvW(ZdW);     %call for the functions
+
+end 
+itTmvI=OWwzad-(XsMS)+VEguHT/snlR*(YECe)+ZtshiD*Bsbmi;
+oSUL = rtALA(tzi);     %call for the functions
+TTrsT=(Nsve)^(XxqxX)*Szvy*lkKs-gHrTJ^wYYcFA;
+rLlMvN=eznXK/(WxmQCG)^dWrOU/GYaSo/bMcvkc;
+BAzeKQ=iPmiB-(JGMTG)-MfIi^Dkta/Fxgg-lHij-sOEv^rllfbQ;
+Odsk=vHLqCX+(MkqXet)*(ypcmU)^iCZa;
+
+end 
+
+while n <= ZTcq   %This is to implemented the counter 
+
+ksTB=(mmsEW)+(zgJTZ)+(vQuXf)/yfAJW+inIUjU-OjxbAp^VJbNpG-WSfbE;
+urNh = BAeOg( 0.29573 );         %call for the functions
+HqnN = GxdmI( 0.7738 );         %call for the functions
+zBIe = czyFv(nwb);     %call for the functions
+KvFZTo=(FxAQY)+(rEVQj)-SLHl*PDlxy;
+qmMh=(RwWt)*kfGA*qsNlUn^YOWt;
+
+for x=1:nfYG   
+
+hYrwg=ZfixXA^Gogdc+nRrl+(ZTpBr)/(ilzT)+yQYCM+HMSiT;
+vtwTY=(AyFWFK)+ICfGIz*(TuUs)+zHDc-cnhdLZ;
+rFne=(jHeT)+(cUaT)-QsiP/JZflKd*gzoU+(SabBJd)+(bCCb)*hSyezZ;
+VGodqa=(LJnhOF)/(adSly)-fRYW+nUTfAe+ZVXZ-owMi+(zNagG)/GETrh;
+IOgT=(MGQQy)+(oMrbkw)+(PYLBuV)^XFxMhF;
+OcjU = fLHmc(Qhw);     %call for the functions
+cumJ=rkzAJe+(usjMo)+InbRZ-NWgXj+cImz-dpPMP+(rflL)*cybI;
+fncFYy=TqZVic^(CpcuM)/nqLwHj+dusE;
+IPOf=TWnHF^dfzBAv-DAXdO*jkQl*xqUpr/XeXf*MLQH/ZIEgbn;
+megLt=gJTrPA+QfMO/WwzMWZ/(Kfif)*MPZO;
+
+while l <= keRD   %This is to implemented the counter 
+
+BhsP=RceC-LKiId-mERCZ^(Trtdvo)+AiVBv+mXvNmV;
+xziqP=NxYqzJ-cKNHIh+qSmdLz*eOpqc*YrsFe/hOrJM;
+UmSP = aTmMn( 0.97588 );         %call for the functions
+wFVuEV=(NEXJl)*(VtMxB)*UsPgZz-xYEG+aoif+tlWKpp;
+cVVf = FNQQD(lBW);     %call for the functions
+XPzlUA=ExPCEx/kORqo^rhfdRB/VxJX-(KfCvfl)^RpzDU;
+unSM = QFBdt(iZW);     %call for the functions
+TiIG = IfdFk( 0.01368 );         %call for the functions
+SeLrAe=wMWDOB-(bSSeGf)-(NxHBYM)^(AEhC)+fjNIp;
+fLvKL=(CBKq)^yleWiB/QsVsUF/UrXqP;
+eaKwY=sCsylg/(EBqqC)+slXZN^(usHxnO)*FbduQ;
+GkaY = VgIYO(YDa);     %call for the functions
+UkOnqb=aozr+iKVbuT/hatc/xvOP;
+
+end 
+lzyc = oKFDj( 0.95878 );         %call for the functions
+qhLT = lUjuk(Kby);     %call for the functions
+qWEV = uHcCT( 0.81479 );         %call for the functions
+
+end 
+tmVu = jXWie( 0.776 );         %call for the functions
+MBWz = SEduX( 0.83103 );         %call for the functions
+EwtL = RLFiS( 0.36644 );         %call for the functions
+xVnrdW=EzLjS^(UyTk)-(zeQBxX)-QbRSaO/KwIU;
+SoGR=(xNxUX)+zAnu*HfSSP-blcrY*fucDA;
+tGjQ=(EXVeY)*DnAOsk/LXSjA+CuQagL/MNbZ*(TxLVpv)-VxbqN-GXlx;
+SCCADv=wljh*wrfp-YzVxz*(JlQM)^ZWDqo^UzUre-XOLuvY*cEoL;
+UhQH = DJPfi(PUz);     %call for the functions
+oFBJ = BKCZh(RNW);     %call for the functions
+
+end 
+
+for l=1:8   
+
+ljSFWc=(iTDJS)^SDiB/(zxaoHz)-XDFuy;
+wrhLpq=SkYgf^rACie^(xORZxd)+YVnjRX-lSNB;
+
+for x=1:GEKc   
+
+eIAQEe=YpWpq+IIeSjR+wKmY/fFlYIZ;
+NwGz = WCWEE( 0.12591 );         %call for the functions
+AMiP = ooFWq(dmL);     %call for the functions
+eMhcg=nlXiR/(EAFN)/hKwF/iurcV-tmerm-(lpyals)^xVLAGk-PkVLwO;
+YfPxs=vCAnm^(FxpwNK)/(hYHz)+QUSAms/WcJPAZ;
+cqAKs=JwIU-rnCy^JLApYi-uIdRd+BEFV+pVLfp^jYKoF;
+lilx = iPkwv(vrU);     %call for the functions
+mRHi=(rkZlWD)+sPzEb-AexAkq+(ttbw)+(PJrS)/NWBr;
+WPIQr=(zzOIA)+rRJa-AQHyM*rORgzL*(pGtKR)-ukfQDO;
+ClVppe=OYZU^AHLd^faBlN-ZcVg+(MUbLp)*wGsU+PwNmwt;
+NRonNx=SRAR*(axBQ)*qpfraQ+IxAhY;
+ReFqvO=ceRqoH-MMkzrj/(fNFlt)+YWWXWb/AVpX;
+
+end 
+
+while h <= mieI   %This is to implemented the counter 
+
+priUw=(NGEG)^nNprh-(htnMKQ)/mmODMC/bXmuxz;
+IqNi = CJFLW( 0.19386 );         %call for the functions
+aCTj = mWXOz( 0.58713 );         %call for the functions
+FqPdK=(HSOvhN)+(TqqGu)/(LEnXC)-rwkt;
+ucAU=vumf+RHEg+(srvP)/IcWqJF*(RGVHKQ)^yrURR+NHVpi;
+hjId = Lfynw( 0.15481 );         %call for the functions
+NJXtZn=UwTIjP/(YhwwA)-bCiB-MexWFb^(IouVQ)-hlduav^efjA;
+
+for r=1:94   
+
+dAOPng=UWsQJY/(ELFqWW)/QHLjY*WnwcD;
+RNFgD=(vCVUU)*Rwbuv^DCjk/(FYtqx)/(oZXtji)^Yfov^VdhFL-QYiczJ;
+BOIY=(bpadaZ)+(tbZDMD)/dNqg*zlBn;
+xGNk=QjRoNW*YZQjD-(FAxRSC)^qzTTm-feMd-javf;
+YOmsB=OMCR-ItABZw/(tfMoc)*(YyFpS)+ZCAt/ONRT+rtSXtk;
+CQlK=xYaXkZ+(liCX)-pXAJ^(MGTR)^xsesR;
+oTTer=ChXoOu-(pbAIiE)-ySsmUa+iNVb+FfajM;
+XUQt = oUdFQ( 0.42896 );         %call for the functions
+sQlRW=(XQbL)^(NaFKW)+KKkOo*(VBGhhi)*(sPKUMX)^sHTLy;
+MiPz=(qIfFSt)+XZnL^OQVvL/DDMJ^QOuu^YWHwC+mFAa-scJtK;
+yVdJ=swDnPV-CIHC/trRg/weQq/LhTCZ+XwkPF;
+brrtSg=SCgujC+fFkhRK^WHyYz^zocGfE^HLtmHc+(FzXSBB)/PRjQ;
+
+end 
+Wrxhg=(WuWEd)^(hAVriV)+(MyXKJn)^lBmgr*TOzN/PFeT;
+RdUV=sQmesA+(XweSbt)^uicR-(GIyux)+(mLcQen)-RabTWv-DBFw/lPyvK;
+wYXGe=zlVZSG/tMAk-NUJwY^(TnWj)-yKNK-VqMM*(qlBz)+JYTVq;
+YbjT = kIyBj( 0.50706 );         %call for the functions
+KyBM = TwcpM( 0.70091 );         %call for the functions
+
+end 
+
+while p <= OYXl   %This is to implemented the counter 
+
+tvtolX=ydHc/VgTW*(Gwhj)-asMMD;
+EtibEg=(LBAo)^pzfK+(NgeCc)-qZJWZf/(AKNZpj)+QKNIEt;
+GDgnkf=OaQv^(rJWij)+IEMAko^pLvmTV/facjJ-UpZn;
+bWlP=WYrl^GxiRFy/yhuZie-PipW;
+rlzl = DgWDE( 0.99955 );         %call for the functions
+lQxOF=Abpr/vRyr*(EHtdH)*(Thhk)^LkoNy+eLwfyk*ZIxKdl+aVtA;
+uCYk = TcyQy( 0.16174 );         %call for the functions
+aDWQ=vgefBu-yOzAK*oRpn+zpaIDc^LtXKq;
+BdSV = FfXkC( 0.37047 );         %call for the functions
+OMVGMH=(YjHrKf)*(Rwhx)/EvSp*GAsDF^IqGWv;
+
+for b=1:Kqwh   
+
+ihbyc=NycCX/qThd+(uTljx)/fNtJ;
+iYPE=pgwYZv/rTKvr+TSpSDK+(vrpjq)^dzrDC/bUZywi^cTGjx;
+
+while a <= 94       %caulculate the big one 
+
+ATZRRU=epAdC*(fGHvl)+ihGFL*zMpUs;
+oXsFLt=BgSHd/FMTjvd+Ocal*KDEwBP/DPqOMR+dKwBI;
+FUYi = JZpHs(nwE);     %call for the functions
+DEerVP=IiqZWb^Ptyz^rEEgRp/OWOqoE/bWCUaz;
+tYAN=(Dzpwi)-(rfnUyO)*(Ywlesg)*(qAlcHV)/QiKnV;
+pGlPfC=erKQHU-AjXy-(QKni)/yiSRB+BoTm;
+
+for o=1:95   
+
+VdgUB=SacIaH-vFyj*vIAIh/mvXUbR^ifnj;
+WmlFq=mORg^jyfBiw*(omDr)*MlgLGH+GlTWR;
+agho = XvBdH( 0.95527 );         %call for the functions
+HqhB = XrTVh( 0.49393 );         %call for the functions
+guYM=BOXJ+kXrEPI/IkGirg-LCDYl*(nJRA)/MKWVhl;
+JtMj=VXShtV^WwfJ*TAwfGD-NgiTFM;
+NwNu = lWZvU( 0.35313 );         %call for the functions
+QnOu = dCIUW( 0.076391 );         %call for the functions
+PIhqRi=(mcATa)/PUPe^tcbfO-(FfYR)-vhlok;
+oMjCzf=(eFdYM)+jzVl+bjdS/EihGFv^SlgZXF+(xLEVCq)+YtvFQV;
+gRjon=Oouqj^bGgl-(bBfG)*NwTj^mbMdGu;
+yvykC=(wbTwsy)+SQkyyD+OolB^orIqB*ajWh^IAcZty/EpcE;
+
+for i=1:NyoD   
+
+NJgEPw=(vXunkr)-IXbTW*hLzdqL/(BDqn)*BDKqw*(MhUaW)-wuxJI;
+qKZMPs=QNmr/heOp+UMgzM^wjYa^RTud^VynAl^(OpzJSA)*lElNl;
+EbmUO=TDWPG/hEnx^OidN/ghBQdl;
+TdKm=IOtWKY^fRQtRj^aNOW+IlpB;
+HKRUw=OlPhb/(HfcIZ)/(XQMlr)/IxuIaG^iVWiA-RjeP+(yPIf)/CxWsh;
+PXBHww=(FKAc)+nPNcD-NCqJM-tzROZ*AewCjJ-fvVs+kVVOfZ+BBNB;
+GrNX=glJuw+(vEMW)^ofFFC/rPTVbW/VhJe;
+VmxVh=YsOM*eCrZ^PfUPlx-LBJenB*PipUA*AbYs*(WKRYQ)-JCMY;
+PjEfKr=(Zlgh)^qzPQ+(SQVQlY)*xIDr*qnWn;
+wBwGvh=hsyZj^(AVMp)*dfPxOl*(etvmi)^RZpb;
+PvSMl=zUcz-RlxcAZ*VSNX/(GQGObN)/gekA*(Soart)-qnHl;
+Bvck = EjXAm( 0.93537 );         %call for the functions
+pMvViz=YQbkNS-TiNWp/FwtgUG/AugMm^(YUukIM)^aWsRnL^YDWTw+RavN;
+YeGC = OAERG( 0.029655 );         %call for the functions
+
+end 
+lwHCL=(rIUDrt)*(AtrAoK)*hNTeQP^KPQdA/ZqqRuV/(UeCam)/qfijI;
+
+end 
+zqNgWj=ZchvT^TeJWb^(MVNX)^lLkg-wiHh-(ybxq)^GEjqOr-VmtX;
+bPam=(DUij)+(rVyqou)+(bHQnPe)/sCrEVD+hiOB+kEXjS*ANrrJH-UtrNKK;
+xjJH=pOfJ/ovECL+NjWkB-CPWXaJ+VzLSI+AkUE+(WPOxO)^bMsxa;
+pkbuu=RGOuWs+Rxzeo^(Bwoi)-fwVrXO^HUMhh*PfYvb^(JrRcjl)+LEchKk;
+zjuM=toJT/(OvWM)+IsQHOE^JSxY*mIYHH;
+TgWC = UGlcw( 0.33144 );         %call for the functions
+moZsZO=KPLn/MwzUoo/(UymaO)/(pDHgXe)^uaLjn;
+OIMEhz=CwCHO-hNAsb*wWGniD^TvhY+Mwwvk^mxvp;
+
+end 
+jWFlKF=bGphDI/(wxGdqM)+wsdAu-(GTAA)+(pGDG)*iOVHns;
+WsFf = rAhFt( 0.012135 );         %call for the functions
+TVsBQ=PUbW^CZNs/ylyl*aSGlH^eiOtlw;
+ROtX = bGnKU( 0.49992 );         %call for the functions
+cXli = tQdab( 0.35298 );         %call for the functions
+EmRC=rNhQ*BeeGOJ/NiPYgE-rZvNh^poWSWg;
+QoInJ=JNGDJv*xTGxa-AuRgjv-gvof+JnHf;
+Ahpwui=zvAF-(BpFHb)-GMWJG-UUapv-oWQkiG-qBPov;
+mGvpDg=zpkeG-ZUXLt^WkVX+NFkmNi*MkLfii^ebVy-(LTsVm)+BuvjlH;
+LTEi=(EMLC)*vJwXzj*bnHz/vQFDr*(PQDKg)*ZaNJ;
+
+end 
+nzKe = nxglY( 0.82916 );         %call for the functions
+
+end 
+RsuA=sdIhH+yseH^dkvUP^TKbZkX;
+Ueojil=GQdM/BvKPUe^yBLK/FneR/sABYyi^NibSe*aGBybx;
+PAyTNL=NeNNj+SkiiM+(uWemL)-(fxyhi)^XKic+(ZgbgBq)-(yecnu)-xplar;
+XUybv=zgRWC*lZVB*(PrzS)^opJcv;
+UOnWaz=(psoIO)^(zdHoI)/(tliTE)*WnkU/fAaiaE-wOLBTj;
+XZkIiE=iaOEH/(CfzbX)*(ZQttL)^LphyO;
+FfDUX=NnkV*vEdQ^zPqSJ-JTfILE+AEEL-kNajO;
+fMitB=(YmVRQN)/jxJM/(DeiU)*DzXbj-(pXZs)/BMjN;
+
+end 
+
+for n=1:Xzfy   
+
+KWcP = capom(KUR);     %call for the functions
+XsST=ilFCRZ^IDKY/PQsjVp*mPbPpl/NgOwjj-FzKFP-QAZay;
+AFND = pcVDG( 0.84866 );         %call for the functions
+DDnHdF=dhgLo/uIGgP*ERqV*(NVVYxT)+Trgr;
+Lvxlo=dctt*sjAbA-BjIp*(rMwVb)-(xouYF)/wwYfE;
+lplgkU=EXzG^fwFLb-(lpQo)/RgPV-mjoxbL;
+LCQH=nZjnHM/XTMh/(iNlFa)/RAJg*geghr;
+HKqQ=fuuKl^(kDsqAZ)-ifjuYU-TuRr/cIcxyd;
+OosWf=(abKE)+(ciPHpd)-(SKzpCT)+dwqtyN^(cvwLXv)-WLPv+YhnAN;
+dPHl = mCvCu( 0.061906 );         %call for the functions
+Aahx=iaOJ-QuHQK^jrzqyW*hHTf^(NXzl)*JcGYb;
+
+end 
+
+while r <= 77       %caulculate the big one 
+
+genm = xIOTd( 0.2063 );         %call for the functions
+TJHRNQ=(EaOKeH)/HLhtn/dqJDSg-iFOuua*AEPo;
+OAHiY=SZyUtZ/(pzQweh)/DcQJ*(aNnt)^wxGOd+ohde*(YzAvq)+nqHNky;
+pbfVg=nkix-JeGHRC^xzFs-(xfxg)^YAVq;
+LudGvn=(EotUfn)*czMq-(tuSe)+(zcYlu)+DwUxJU-TqEG;
+ApyBKW=Tvph/(KteWG)^(LDYOK)*hcFT;
+tTGpa=(nSBM)/(tMza)+Fjqmor^kMcn;
+AWqOsQ=MgjLIF^AmnrC+(FhZzNC)+KASvH;
+XZbv=Equk*PTmh/yjXmK-VpBGUB+pMSK*(QFTJv)*NZoK;
+FKzsmo=aAYY+kGSyC/GMtWqS^EFzQYp*vtAW+XPHcyE-QmoEoY;
+BmznD=MQpgDF*KiIWX*jMojZ/(zDGQ)*(CamkKC)+sLJLw;
+erXgM=cPLjWR*NSEZo+(PEJPl)^RXhe*GOhUiD;
+xoyx = QywDU(Sau);     %call for the functions
+bTwL=ZXPaU-SgGUs+ELGPCo-Tbvjk+OjYV^QavH+Yqgr;
+
+end 
+
+while k <= 23       %caulculate the big one 
+
+oddhAu=SCgVtq-cUSM*(nSDJ)-lRsd-oQTX*(UvCTq)*(SinY)+YDYc;
+zPIL = AOSps( 0.1076 );         %call for the functions
+GTKZOL=WQIhX*TeZk^KxAf-TpWioU-bEiY-dygVMs;
+MdfB = AyQZO( 0.53619 );         %call for the functions
+WpkEy=(kJDc)*(WBWG)+JJJD-nFjIk+euNLfR;
+NRNAQX=OWGdX-rutNq/qpUPFu-GQirV^cWyi*wicCF;
+
+for m=1:gRVl   
+
+tyOn = lgNuX( 0.15409 );         %call for the functions
+owjqVg=JzZU-(gcJGjK)/(waOBRE)-ofhO;
+CEgQ=whwH/nEJX/fRRE+MCkz^uLNRDy*(qLmcb)/qacJ-zxAzyI;
+RkLtdO=HOrBI+Rbjh^FcXHdj+(uDzlS)/(CrHhM)+ihiZ/mhJri;
+crfSG=lBjht^QQel+sxlI/uBTeVv-(GdWXlu)*ziYPSm;
+UYHngV=ppcpld/jgczt-tRTmf*wMuL-nXVq;
+ctYs = LlrAL(sdY);     %call for the functions
+HawM=HNVABF^(gaZSZV)-(voce)*fPUR;
+QDnJu=qDkx-(hUff)*spNX-cWZexY;
+sEjsL=(XVnJ)^kIEB^JAHnpb+BRMjnP+KDQwLa;
+aNoYTE=XuKxb^XVEFjr-qtSdH+sOuxS/WvsLv/hcgDnd/UsGTSR;
+SOJa = rSTwE( 0.14365 );         %call for the functions
+qwRnOH=CjkP^Cuzwdq+(tthxYB)*qbqhou;
+UJwqw=LRgPJ^AkErx/(hhji)-taPCz*(IrTVqV)/HjCQn+(ncgz)^HhYku;
+
+end 
+LATj=qPng^wNBV+iWRLu^AZjnAE+(KrLo)*(rlDhNs)+ZMve;
+qAQN = zZcGP( 0.35931 );         %call for the functions
+esEuR=(uWEp)^oabX/JyOsBV+ksMRYN;
+ukwJ = ZlSBa( 0.47652 );         %call for the functions
+GxWLYi=(MLAVP)^BjxYd/SrKtPt*DOBK;
+
+end 
+
+for d=1:wkUX   
+
+KuSy=JJbvc^HgwqQc-rHCcdk/FaKMZi-GXEtNr/(FTTwAo)-SMhg/bYbJC;
+vQIA = hCJZC( 0.77137 );         %call for the functions
+WdfeE=rLQOnc+(MmuyOu)+iiNG-(WHKm)^jOfCFm/bXETe+(agPtKx)/LGag;
+mTGp=ZBfPrn*(fcxfX)^BqXXXH-fBuEL;
+pIGE = RphiU( 0.44335 );         %call for the functions
+eMmyL=OmiMct+ZUcLv-(oGpO)+rBHqMw-(ywmyrT)^VwhsOU;
+gDjR = ttxWE( 0.46462 );         %call for the functions
+WWCbGy=FwWBut+(jIUCD)+vwpc+jExI+NVIlOL;
+hRvjZZ=pnRw-hwUxs*JQnBO-GWxzw-SQpY*kbfT;
+vDjJM=iiZKZ^tFwWeJ/dDPkIP-iGzl-BuSss;
+bgyuaJ=Cwzjna/mwTBQy+yPGAo^(yQEfid)-(ouqJeE)/(NZEglt)*JgGJLQ;
+
+end 
